f7f5cd985a139957741fb4a5e7e7ff0ae8d34540
Everlyn <gachuieve32@gmail.com>
2023-09-30 14:43:43 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit",(e) => {
+  e.preventDefault();
+  buildToDo(e.target.new_task.value);
+  form.reset()
+    });
 });
+
+function buildToDo(todo){
+  const li = document.createElement("li");
+  const btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete)
+  li.textContent = `${todo} `;
+  btn.textContent = "x";
+  document.querySelector("#tasks").appendChild(li);
+  li.appendChild(btn);
+  
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
